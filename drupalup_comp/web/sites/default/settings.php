ad9d26949240be5ae94e6bcb5f23d722f8664d7d
YasminHansen <yaa-h@hotmail.com>
2019-05-17 15:13:48 -0300

    Configurações corrigidas

--- drupalup_comp/web/sites/default/settings.php
+++ drupalup_comp/web/sites/default/settings.php
@@ -1,6 +1,7 @@
 <?php
 $settings['hash_salt'] = '_UzIAFwbXeKS48KtIXf7w3YbJIjCOPbsKy3pNfUB7hjOhvFWcEuTxF6bk4Px96hakCRQpWzwfg';
 $config_directories['sync'] = 'sites/default/files/config_txJp1pMqpgbl4LCHAc6JbG8F88nguIBjamd1eH5-WHYj_buVovJjYctWY_7wxlkWatOzqKlEZw/sync';
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
 
 if (file_exists(__DIR__ . '/settings.local.php')) {
  require_once(__DIR__ . '/settings.local.php'); 
