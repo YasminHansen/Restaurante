43dd0e76fe51495abacd0db5c857548798833842
YasminHansen <yaa-h@hotmail.com>
2019-07-01 13:13:45 -0300

    Algumas alterações no paragrafo de Texto Destaque, criação da página de confirmação de e-mail

---
+++ drupalup_comp/web/modules/custom/cards_de_destaque/src/Controller/CardsDestaqueController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\cards_de_destaque\Controller;
+
+class CardsDestaqueController{
+  public function cardsDestaque(){
+    return array(
+      '#markup' => 'Feliz páscoa!!!!!'
+    );
+  }
+}
